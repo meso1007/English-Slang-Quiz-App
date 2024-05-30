62de1e9109bdf4f98ce1c1fa5d553b027cc2f55e
shoyaaaaa <diegoshoya2017@gmail.com>
2024-05-29 17:04:28 -0700

    Initial commit

---
+++ quizreal/explainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  explainViewController.swift
+//  quizreal
+//
+//  Created by 堀内翔矢 on 2024/02/04.
+//
+
+import UIKit
+
+class explainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
